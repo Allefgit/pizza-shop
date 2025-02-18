2ec6e516d642ac1d944c638d8f9ebfb372ca7574
Allefgit <allefgit@gmail.com>
2025-02-18 19:29:06 -0300

    corrigindo o comando de test

--- test/dashboard-e2e.spec.ts
+++ test/dashboard-e2e.spec.ts
@@ -4,7 +4,7 @@ test('display day orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
   await expect(page.getByText('36', { exact: true })).toBeVisible()
-  await expect(page.getByText('-5% em relação a ontem')).toBeVisible()
+  await expect(page.getByText('-5% em  relação a ontem')).toBeVisible()
 })
 
 test('display total month amount metric', async ({ page }) => {
